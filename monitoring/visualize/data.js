f5959cf68083e90ac34e32089f09d3905cb2f1c4
Evyatar Segal <>
2020-04-06 22:33:58 -0400

    Move the data file to .gitignore

--- monitoring/visualize/data.js
+++
@@ -1,9 +0,0 @@
-data = {
-  "floors": 10,
-  "initial_floor": 1,   
-  "events": [
-    {"ts": 0, "floor":  1, "event": "request", "rider":  0, "source":  1, "destination":  10},
-    {"ts": 0, "floor":  1, "event": "request", "rider":  1, "source":  1, "destination":  5},
-    {"ts": 0, "floor":  1, "event": "request", "rider":  2, "source":  1, "destination":  5}
-  ]
-};
